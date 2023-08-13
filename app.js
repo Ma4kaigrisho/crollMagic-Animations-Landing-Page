8c088213eef281b253793bb3755d6d3fd7c21920
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-08-13 23:01:13 +0300

    fade in

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square",{
-    duration: 3,
-    scrollTrigger: {
-        trigger: ".square",
-        start: "top 30%",
-        end: ()=> `+=${document.querySelector(".square").offsetHeight}`,
-        markers: true,
-        toggleClass: "green"
-    }
+$(document).ready(function(){
+    var controller = new ScrollMagic.Controller();
+
+    $('.project').each(function(){
+        var ourScene = new ScrollMagic.Scene({
+            triggerElement: this.children[0],
+            triggerHook: 0.85,
+            // reverse: false
+        })
+        .setClassToggle(this, 'fade-in')
+        .addIndicators({
+            name: "fade scene 1",
+            colorTrigger: "black"
+        })
+        .addTo(controller);
+    });
 })
