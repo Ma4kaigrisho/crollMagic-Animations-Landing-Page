27f3f0386021a02a1f7f6fa4e6b098fba8bc444c
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-08-15 08:39:21 +0300

    para

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 $(document).ready(function(){
     var controller = new ScrollMagic.Controller();
-
+    //pin
     var pinScene = new ScrollMagic.Scene({
         triggerElement: ".first",
         triggerHook: 0,
@@ -12,7 +12,7 @@ $(document).ready(function(){
     .addIndicators()
     .addTo(controller);
 
-
+    //fade in
     $('.project').each(function(){
         var ourScene = new ScrollMagic.Scene({
             triggerElement: this.children[0],
@@ -27,4 +27,22 @@ $(document).ready(function(){
         })
         .addTo(controller);
     });
+    //parallax
+
+    var parallaxTl = new TimelineMax();
+    parallaxTl
+        .from('.content-wrapper', 1, {autoAlpha: 0, ease:Power0.easeNone})
+        .from('.bcg', 1, {y: '-30%', ease:Power0.easeNone}, 0)
+        ;
+
+    var parScene = new ScrollMagic.Scene({
+        triggerElement:".bcg-parallax",
+        triggerHook: 1,
+        duration: "100%"
+    })
+    .setTween(TweenMax.from('.bcg', 1, {y: '-30%', ease:Power0.easeNone}))
+    .addIndicators()
+    .addTo(controller);
+
+
 })
